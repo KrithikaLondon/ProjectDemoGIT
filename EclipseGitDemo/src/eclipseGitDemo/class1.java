da3dd0412c64038e31a1df6b722408565cb6f9ca
krithikalondon <krithikalondon@gmail.com>
2021-06-06 12:30:08 +0100

    "modified class"

--- EclipseGitDemo/src/eclipseGitDemo/class1.java
+++ EclipseGitDemo/src/eclipseGitDemo/class1.java
@@ -3,7 +3,7 @@ package eclipseGitDemo;
 public class class1 {
 
 	public static void main(String[] args) {
-		System.out.println("welcome to git");
+		System.out.println("welcome to git 2"); 
 		// TODO Auto-generated method stub
 
 	}
